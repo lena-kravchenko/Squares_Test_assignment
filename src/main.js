602d86f26bef6659dc24916eb767ae57cebfb492
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-20 22:47:56 +0300

    Styles edited and some jd added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// TODO: Add JS
+'use strict';
+
+const tbody = document.querySelector('tbody');
+const rows = document.querySelector('tr');
+
+const appendRowButton = document.querySelector('.append-row');
+const removeRowButton = document.querySelector('.remove-row');
+const appendColumnButton = document.querySelector('.append-column');
+const removeColumnButton = document.querySelector('.remove-column');
+
+const minCountOfRowsOrColumns = 1;
