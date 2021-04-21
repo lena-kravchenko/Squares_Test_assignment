f653c3d35447f5cd8af416976aa0ee0d75e9669c
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-22 00:07:19 +0300

    Main functional done

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 
 const tbody = document.querySelector('tbody');
 const rows = document.querySelector('tr');
-const cells = document.querySelectorAll('td');
+const container = document.querySelector('.container');
+const paddingOfContainer = 60;
 
 const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
 const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
 
-const minCountOfRowsOrColumns = 1;
+const minAmountOfRowsOrColumns = 1;
 
 function appendRow() {
   const row = tbody.children[0].cloneNode(true);
@@ -21,8 +22,9 @@ function appendRow() {
 
 function removeRow() {
   tbody.lastElementChild.remove();
+  removeRowButton.style.display = 'none';
 
-  if (tbody.children.length === minCountOfRowsOrColumns) {
+  if (tbody.children.length === minAmountOfRowsOrColumns) {
     removeRowButton.disabled = true;
   }
 }
@@ -41,8 +43,9 @@ function removeColumn() {
   [...tbody.children].map(child => {
     child.lastElementChild.remove();
   });
+  removeColumnButton.style.display = 'none';
 
-  if (rows.children.length <= minCountOfRowsOrColumns) {
+  if (rows.children.length <= minAmountOfRowsOrColumns) {
     removeColumnButton.disabled = true;
   }
 }
@@ -57,25 +60,22 @@ function hideRemoveButtons() {
   removeColumnButton.style.display = 'none';
 }
 
-function moveRemoveRowButton() {
-  removeRowButton.style.position = 'relative';
-
-  removeRowButton.style.top = tbody.clientHeight / 5;
-}
-
 appendRowButton.addEventListener('click', appendRow);
 removeRowButton.addEventListener('click', removeRow);
 appendColumnButton.addEventListener('click', appendColumn);
 removeColumnButton.addEventListener('click', removeColumn);
 
 document.addEventListener('mouseover', event => {
-  if ([...cells].includes(event.target)) {
+  if (tbody.contains(event.target)) {
     showRemoveButtons();
+
+    removeRowButton.style.top = event.clientY - event.offsetY + 'px';
+    removeColumnButton.style.left = event.clientX - event.offsetX - window.innerWidth / 2 + tbody.clientWidth / 2 + paddingOfContainer + 'px';
   }
 });
 
-document.addEventListener('mouseout', event => {
-  if ([...cells].includes(event.target)) {
+document.addEventListener('mouseover', event => {
+  if (!container.contains(event.target)) {
     hideRemoveButtons();
   }
 });
