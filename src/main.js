cdb173d42ee40f037f5b3c35f2f9d0bc5b2fb5d3
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-21 11:21:19 +0300

    Show Remove buttons functions added

--- src/main.js
+++ src/main.js
@@ -2,30 +2,31 @@
 
 const tbody = document.querySelector('tbody');
 const rows = document.querySelector('tr');
+const cells = document.querySelectorAll('.cell');
 
 const appendRowButton = document.querySelector('.append-row');
 const removeRowButton = document.querySelector('.remove-row');
 const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
 
-const minCountOfRowsOrColumns = 1;
+// const minCountOfRowsOrColumns = 1;
 
 function appendRow() {
   const row = tbody.children[0].cloneNode(true);
 
   tbody.append(row);
 
-  removeRowButton.disabled = false;
+//  removeRowButton.disabled = false;
 }
 
 function removeRow() {
   tbody.lastElementChild.remove();
 
-  if (tbody.children.length <= minCountOfRowsOrColumns) {
-    removeRowButton.disabled = true;
-  }
+//  if (tbody.children.length <= minCountOfRowsOrColumns) {
+//    removeRowButton.disabled = true;
+//  }
 
-  appendRowButton.disabled = false;
+//  appendRowButton.disabled = false;
 }
 
 function appendColumn() {
@@ -35,9 +36,9 @@ function appendColumn() {
     child.append(column);
   });
 
-  if (rows.children.length >= minCountOfRowsOrColumns) {
-    removeColumnButton.disabled = false;
-  }
+//  if (rows.children.length >= minCountOfRowsOrColumns) {
+//    removeColumnButton.disabled = false;
+//  }
 }
 
 function removeColumn() {
@@ -45,12 +46,25 @@ function removeColumn() {
     child.lastElementChild.remove();
   });
 
-  if (rows.children.length <= minCountOfRowsOrColumns) {
-    removeColumnButton.disabled = true;
-  }
+//  if (rows.children.length <= minCountOfRowsOrColumns) {
+//    removeColumnButton.disabled = true;
+//  }
+}
+
+function showRemoveButtons() {
+  removeRowButton.style.display = 'block';
+  removeColumnButton.style.display = 'block';
+}
+
+function hideRemoveButtons() {
+  removeRowButton.style.display = 'none';
+  removeColumnButton.style.display = 'none';
 }
 
 appendRowButton.addEventListener('click', appendRow);
 removeRowButton.addEventListener('click', removeRow);
 appendColumnButton.addEventListener('click', appendColumn);
 removeColumnButton.addEventListener('click', removeColumn);
+
+tbody.addEventListener('mouseover', showRemoveButtons);
+tbody.addEventListener('mouseout', hideRemoveButtons);
