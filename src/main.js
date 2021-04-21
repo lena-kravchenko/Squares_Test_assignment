c9c5567202f51fd33f212cccaf607d2e95d030f9
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-04-21 10:47:06 +0300

    First functions added

--- src/main.js
+++ src/main.js
@@ -9,3 +9,48 @@ const appendColumnButton = document.querySelector('.append-column');
 const removeColumnButton = document.querySelector('.remove-column');
 
 const minCountOfRowsOrColumns = 1;
+
+function appendRow() {
+  const row = tbody.children[0].cloneNode(true);
+
+  tbody.append(row);
+
+  removeRowButton.disabled = false;
+}
+
+function removeRow() {
+  tbody.lastElementChild.remove();
+
+  if (tbody.children.length <= minCountOfRowsOrColumns) {
+    removeRowButton.disabled = true;
+  }
+
+  appendRowButton.disabled = false;
+}
+
+function appendColumn() {
+  [...tbody.children].map(child => {
+    const column = document.createElement('td');
+
+    child.append(column);
+  });
+
+  if (rows.children.length >= minCountOfRowsOrColumns) {
+    removeColumnButton.disabled = false;
+  }
+}
+
+function removeColumn() {
+  [...tbody.children].map(child => {
+    child.lastElementChild.remove();
+  });
+
+  if (rows.children.length <= minCountOfRowsOrColumns) {
+    removeColumnButton.disabled = true;
+  }
+}
+
+appendRowButton.addEventListener('click', appendRow);
+removeRowButton.addEventListener('click', removeRow);
+appendColumnButton.addEventListener('click', appendColumn);
+removeColumnButton.addEventListener('click', removeColumn);
